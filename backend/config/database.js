e622e2a4035619968264663ea92988a80eaf09fc
Rio Rivaldo Sinuhaji <riorivaldo40@gmail.com>
2025-03-12 18:58:39 +0700

    update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,8 @@
 // src/config/database.js
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("note", "root", "", {  
-  host: "localhost",
+const db = new Sequelize("note", "root", "diary", {  
+  host: "34.133.192.60",
   dialect: "mysql",
 });
 
